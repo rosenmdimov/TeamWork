99fc31e582f26f027fef809aeb320075d3b5e165
rosenmdimov <rosenmdimov@gmail.com>
2017-05-05 00:59:14 +0300

    GitGuiCommit

---
+++ BlogUITests/BlogUITests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace BlogUITests
+{
+    [TestFixture]
+    public class BlogUITests
+    {
+        [Test]
+        public void CheckSiteLoad()
+        {
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+            driver.Navigate().GoToUrl(@"http://localhost:60634/Article/List");
+
+            var logo = driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/a"));
+            Assert.AreEqual("SOFTUNI BLOG", logo.Text);
+        }
+    }
+}
---
+++ BlogUITests/BrowserHost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.Seleno;
+using TestStack.Seleno.Configuration;
+
+
+namespace BlogUITests
+{
+    public static class BrowserHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        public static readonly string RootUrl = @"http://localhost/Article/List";
+          static  BrowserHost()
+        {
+            Instance.Run("Blog", 60634);
+            
+        }
+    }
+}
